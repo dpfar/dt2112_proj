1c680d8503e1dee5593726a5bad1eb39e27e7202
Dennis Magnusson <denmag@kth.se>
2021-03-02 19:33:32 +0100

    Add syllable estimate

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ import librosa
 import numpy as np
 from os import system
 
+import math
+
 app = Flask(__name__, static_url_path='/', static_folder='static')
 
 @app.route('/')
@@ -20,11 +22,26 @@ def thing():
   system('ffmpeg -y -i {} -ac 1 {}'.format(f.filename, wav_filename))
   data, sr = librosa.load(wav_filename)
   spectrogram = librosa.feature.melspectrogram(y=data, sr=sr)
-  f0, _, prob = librosa.pyin(data, sr=sr, fmin=65, fmax=2093)
+  f0, _, prob = librosa.pyin(data, sr=sr, fmin=65, fmax=2093, frame_length=512)
 
   return jsonify({'voiced_prob': np.nanmean(prob),
                   "f0": np.nanmean(f0),
-                  "stddev": np.nanstd(f0)})
+                  "stddev": np.nanstd(f0),
+                  'syllable_estimate': count_syllables(f0)})
+
+
+def count_syllables(f0):
+  n = 0
+  br  = False
+  for i in f0:
+    if math.isnan(i):
+      if br:
+        n += 1
+      br = False
+    else:
+      br = True
+
+  return n
 
 if __name__ == '__main__':
   app.run()
