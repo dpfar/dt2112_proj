4952c8ac434fb1ff2e0a87f5934c45dba1f142f8
Dennis Magnusson <denmag@kth.se>
2021-03-06 10:18:06 +0100

    Switch to template-based rendering

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, request, jsonify
+from flask import Flask, redirect, request, jsonify, render_template
 import soundfile as sf
 import librosa
 import numpy as np
@@ -9,9 +9,14 @@ import math
 app = Flask(__name__, static_url_path='/', static_folder='static')
 
 @app.route('/')
-def display_page():
-  print('alsdjfalsdkjfasldkfjasdf')
-  return redirect('index.html', code=302)
+def redir(index=0):
+  return redirect('/app/0', code=302)
+
+@app.route('/app/<index>')
+def display_page(index=0):
+  
+  return render_template('index.html', index=index)
+  
 
 @app.route('/thing', methods=['POST'])
 def thing():
@@ -20,6 +25,8 @@ def thing():
   wav_filename = 'tmp.wav'
   # Convert to wav (file is in ogg format, so thing don't work with sf or sr)
   system('ffmpeg -y -i {} -ac 1 {}'.format(f.filename, wav_filename))
+  return jsonify(get_metrics(wav_filename))
+  """
   data, sr = librosa.load(wav_filename)
   spectrogram = librosa.feature.melspectrogram(y=data, sr=sr)
   f0, _, prob = librosa.pyin(data, sr=sr, fmin=65, fmax=2093, frame_length=512)
@@ -29,6 +36,19 @@ def thing():
                   "stddev": np.nanstd(f0),
                   'syllable_estimate': count_syllables(f0),
                   'voiced_percent': float(np.count_nonzero(np.where(np.isnan(f0), 0, 1)))/f0.size})
+  """
+
+
+def get_metrics(wav_filename='tmp.wav'):
+  data, sr = librosa.load(wav_filename)
+  spectrogram = librosa.feature.melspectrogram(y=data, sr=sr)
+  f0, _, prob = librosa.pyin(data, sr=sr, fmin=65, fmax=2093, frame_length=512)
+  return {'voiced_prob': np.nanmean(np.where(np.isnan(f0), f0, prob)),
+                  "f0": np.nanmean(f0),
+                  "stddev": np.nanstd(f0),
+                  'syllable_estimate': count_syllables(f0),
+                  'voiced_percent': float(np.count_nonzero(np.where(np.isnan(f0), 0, 1)))/f0.size}
+
 
 
 def count_syllables(f0):
